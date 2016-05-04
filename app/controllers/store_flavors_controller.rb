0ac2ca314177aae77596284026b0aca7d9ed48c6
Matt <mattgruber61@gmail.com>
2016-05-03 20:09:43 -0400

    Adding a flavor to a store now works.

--- app/controllers/store_flavors_controller.rb
+++ app/controllers/store_flavors_controller.rb
@@ -1,5 +1,6 @@
-class StoreflavorsController < ApplicationController
+class StoreFlavorsController < ApplicationController
   before_action :set_flavor, only: [:show, :edit, :update, :destroy]
+  load_and_authorize_resource :except => :create
 
   def index
   end
@@ -11,11 +12,14 @@ class StoreflavorsController < ApplicationController
   def edit
   end
 
+  def update
+  end
+
   def create
     @store_flavor = StoreFlavor.new(flavor_params)
-    
+
     if @store_flavor.save
-      redirect_to store_path(@store_flavor.store), notice: "Successfully added #{@store_flavor.flavor.name} to store."
+      redirect_to store_path(current_user.store_id), notice: "Successfully added #{@store_flavor.flavor.name} to store."
     else
       render action: 'new'
     end
@@ -23,7 +27,7 @@ class StoreflavorsController < ApplicationController
 
   def destroy
     @store_flavor.destroy
-    redirect_to store_path(@store_flavor.store), notice: "Successfully removed #{@store_flavor.flavor.name} from store."
+    redirect_to store_path(current_user.store_id), notice: "Successfully removed #{@store_flavor.flavor.name} from store."
   end
 
   private
@@ -32,7 +36,6 @@ class StoreflavorsController < ApplicationController
   end
 
   def flavor_params
-    params.require(:employee).permit(:id, :store_id, :flavor_id)
+    params.require(:store_flavor).permit(:store_id, :flavor_id)
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :assignments
   resources :users
   resources :sessions
-  resources :storeflavors
+  resources :store_flavors
   
   # Semi-static page routes
   get 'home' => 'home#home', as: :home
